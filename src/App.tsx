0475e341389c1ee897d227ff9d46aa4c68275d8a
TheHunSaiyan <panczelbarnusdarth@gmail.com>
2025-05-04 14:51:33 +0200

    Login process
    Login process has been implemented

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,16 @@ import {BrowserRouter} from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
 import Routing from "./routing/Routing.tsx";
 import {useState} from "react";
-import {emailKeyName, tokenKeyName} from "./constants/constants.ts";
+import {emailKeyName, tokenKeyName, roleKeyName} from "./constants/constants.ts";
 
 export default function App() {
   const [token, setToken] = useState(localStorage.getItem(tokenKeyName));
   const [email, setEmail] = useState(localStorage.getItem(emailKeyName));
+  const [roles, setRoles] = useState(JSON.parse(localStorage.getItem(roleKeyName) || '[]'));
 
   return <MantineProvider theme={theme}>
     <BrowserRouter>
-      <AuthContext.Provider value={{ token, setToken, email, setEmail }}>
+      <AuthContext.Provider value={{ token, setToken, email, setEmail, roles, setRoles }}>
         <Routing/>
       </AuthContext.Provider>
     </BrowserRouter>
--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,9 @@ import { IFilm } from "../interfaces/IFilm.ts";
 import { IScreening } from "../interfaces/IScreening.ts";
 import axiosInstance from "./axios.config.ts";
 
-const Auth = {}
+const Auth = {
+    login: (email: string, password: string) => axiosInstance.post<{token: string}>(`/api/Users/login`, {email, password})
+}
 
 const Films = {
     getFilms: () => axiosInstance.get<IFilm[]>(`/api/film`),
