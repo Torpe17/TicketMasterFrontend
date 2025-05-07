406f78ea92b56ed0ce94fa55aced0538a7130883
Luksa Laura <laura.luksa03@gmail.com>
2025-05-07 14:24:17 +0200

    inspect ticket by reading qr code

--- src/pages/TicketInspection.tsx
+++ src/pages/TicketInspection.tsx
@@ -1,8 +1,11 @@
-import { Alert, Button, Card, Fieldset, Group, NumberInput, rem, Title } from "@mantine/core";
+import { Alert, Button, Card, Fieldset, Group, Modal, NumberInput, rem, Title } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { IconAlertTriangle, IconCheck } from "@tabler/icons-react";
-import { useState } from "react";
+import { IconAlertTriangle, IconCheck, IconObjectScan } from "@tabler/icons-react";
 import api from "../api/api";
+import { useEffect, useRef, useState } from "react";
+// import "./QrStyles.css";
+import QrScanner from "qr-scanner";
+import { useDisclosure } from "@mantine/hooks";
 
 const TicketInspection = () => {
     const [errorVisible, setErrorVisible] = useState(false);
@@ -21,7 +24,82 @@ const TicketInspection = () => {
         },
     });
 
+    const validateAPI = (id: number) => {
+        api.Tickets.validateTicket(id)
+            .then(res => {
+                if (res.status == 200) {
+                    setErrorVisible(false);
+                    setSuccessVisible(true);
+                    setLastValidatedId(id)
+                }
+            })
+            .catch(err => {
+                if (err.response) {
+                    const status = err.response.status;
+                    console.log(status);
+                    if (status == 404) setAlertMessage("A jegy nem található.")
+                    else if (status == 400) setAlertMessage("A jegyet már ellenőrizték.")
+                    else { setAlertMessage(""); }
+                }
+                setErrorVisible(true);
+                setSuccessVisible(false);
+            });
+        if (opened) close();
+    }
+    // QR States
+    const [opened, { open, close }] = useDisclosure(false);
+    const scanner = useRef<QrScanner>();
+    const videoEl = useRef<HTMLVideoElement>(null);
+    const qrBoxEl = useRef<HTMLDivElement>(null);
+    const [qrOn, setQrOn] = useState<boolean>(true);
+
+    // Success
+    const onScanSuccess = (result: QrScanner.ScanResult) => {
+        console.log("scanned: " + result?.data);
+        scanner.current?.stop();
+        validateAPI(Number(result?.data))
+    };
+
+    // Fail
+    const onScanFail = (err: string | Error) => {
+        console.log(err);
+    };
+    useEffect(() => {
+        if (opened) {
+            const timeout = setTimeout(() => {
+                if (videoEl.current) {
+                    scanner.current = new QrScanner(videoEl.current, onScanSuccess, {
+                        onDecodeError: onScanFail,
+                        preferredCamera: "environment",
+                        highlightScanRegion: true,
+                        highlightCodeOutline: true,
+                        overlay: qrBoxEl.current || undefined,
+                    });
+
+                    scanner.current
+                        .start()
+                        .then(() => setQrOn(true))
+                        .catch((err) => {
+                            console.error("Scanner start error:", err);
+                            setQrOn(false);
+                        });
+                }
+            }, 300);
+
+            return () => {
+                scanner.current?.stop();
+                scanner.current = undefined;
+                clearTimeout(timeout);
+            };
+        }
+    }, [opened]);
+
+    useEffect(() => {
+        if (!qrOn) alert("Nincs engedélyezve a kamera. A scanneléshez engedélyezze és töltse újra az oldalt");
+    }, [qrOn]);
+
     return <Card>
+
         <Fieldset
             legend={<Title order={4}>Jegy ellenőrzés</Title>}
             radius="md"
@@ -33,30 +111,7 @@ const TicketInspection = () => {
                 borderRadius: rem(8),
             }}
         >
-            <form
-                onSubmit={form.onSubmit(async (values) => {
-                    try {
-                        await api.Tickets.validateTicket(values.id)
-                            .then(res => {
-                                if(res.status == 200){
-                                    setErrorVisible(false);
-                                    setSuccessVisible(true);
-                                    setLastValidatedId(values.id)
-                                }
-                            });
-                    } catch (error: any) {
-                        if(error.response){
-                            const status = error.response.status;
-                            console.log(status);
-                            if(status == 404) setAlertMessage("A jegy nem található.")                     
-                            else if(status == 400) setAlertMessage("A jegyet már ellenőrizték.")
-                            else {setAlertMessage("");}                     
-                        }
-                        setErrorVisible(true);
-                        setSuccessVisible(false);
-                    }
-                })}
-            >
+            <form onSubmit={form.onSubmit(async (values) => validateAPI(values.id))} >
                 <NumberInput
                     withAsterisk
                     label="Jegy azonosító"
@@ -69,6 +124,10 @@ const TicketInspection = () => {
                 <Group justify="flex-end" mt="md">
                     <Button type="submit">Mentés</Button>
                 </Group>
+
+                <Button variant="default" leftSection={<IconObjectScan />} onClick={open}>
+                    Beolvasás
+                </Button>
             </form>
             {errorVisible && (<Alert
                 variant="light"
@@ -79,7 +138,7 @@ const TicketInspection = () => {
                 withCloseButton
                 onClose={() => setErrorVisible(false)}
                 closeButtonLabel="Dismiss">
-                Hiba történt a művelet során. <br/>
+                Hiba történt a művelet során. <br />
                 {alertMessage}
             </Alert>)}
             {successVisible && lastValidatedId != -1 && (<Alert
@@ -91,9 +150,26 @@ const TicketInspection = () => {
                 withCloseButton
                 onClose={() => setSuccessVisible(false)}
                 closeButtonLabel="Dismiss">
-                A {lastValidatedId} azonosítójú jegy sikeresen ellenőrizve.<br/>
+                A {lastValidatedId} azonosítójú jegy sikeresen ellenőrizve.<br />
             </Alert>)}
+
         </Fieldset>
+        <Modal opened={opened} onClose={close} title="Jegy beolvasás">
+            <div className="qr-reader">
+                <video ref={videoEl} width={256} height={256}></video>
+                <div ref={qrBoxEl} className="qr-box">
+                    {!videoEl?.current && (
+                        <img
+                            src="/static/images/icons/scan_qr1.svg"
+                            alt="Qr Frame"
+                            width={256}
+                            height={256}
+                            className="qr-frame"
+                        />
+                    )}
+                </div>
+            </div>
+        </Modal>
     </Card>
 }
 
